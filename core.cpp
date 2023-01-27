4e39df5917c8a786fe9ac71175860b0f1d15825c
Tomasz Niewiadomski <74839077+Tomasz-Niewiadomski@users.noreply.github.com>
2023-01-27 01:46:28 +0100

    Fully working match() with memory

--- core.cpp
+++ core.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <stack>
+#include <algorithm>
 
 class Matcher
 {
@@ -116,14 +117,17 @@ class EngineNFA
 		}
 	bool compute(std::string string)
 		{
-			struct Pair{
+			struct Triple
+			{
 				int iterator;
 				int state;
-				Pair(int givenInt, int givenState) : iterator(givenInt), state(givenState){}
+				std::vector<int> memory;
+				Triple(int givenI, int givenState) : iterator(givenI), state(givenState), memory(0){}
+				Triple(int givenI, int givenState, std::vector<int> givenMemory) : iterator(givenI), state(givenState), memory(givenMemory){}
 			};
 
-			std::stack<Pair> stack;
-			Pair stackMember(0, startState);
+			std::stack<Triple> stack;
+			Triple stackMember(0, startState);
 
 			stack.push(stackMember);
 
@@ -131,6 +135,7 @@ class EngineNFA
 			{
 				int i = stack.top().iterator;
 				int currentState = stack.top().state;
+				std::vector<int> memory = stack.top().memory;
 				stack.pop();
 
 				if (endState == currentState)
@@ -144,18 +149,36 @@ class EngineNFA
 				{
 					int toState = states[currentState].stateTransitions[c].toState;
 
+					// Check if matcher matches the input character
 					if (states[currentState].stateTransitions[c].matcher.matches(input) == true)
 					{
+						std::vector<int> copyMemory = memory;
 						int nextIterator;
+						// Check if the matcher is an epsilon
+						if (states[currentState].stateTransitions[c].matcher.isEpsilon())
+						{
+							// Checks if toState is in memory vector
+							if (std::count(memory.begin(), memory.end(), toState))
+								{
+									continue;
+								}
+							copyMemory.push_back(currentState);
+						}
+						else
+						{
+							copyMemory.clear();
+						}
+
 						if (states[currentState].stateTransitions[c].matcher.isEpsilon())
 						{
 							nextIterator = i;
 						}
 						else
 						{
-							nextIterator = i+1;
+							nextIterator = i + 1;
 						}
-						Pair newStackMember(nextIterator, toState);
+
+						Triple newStackMember(nextIterator, toState, copyMemory);
 						stack.push(newStackMember);
 					}
 
@@ -168,29 +191,41 @@ int main()
 {
 	EngineNFA nfa;
 
-	std::vector<State> states_vec{State("q0"), State("q1"), State("q2"), State("q3")};
+//	std::vector<State> states_vec{State("q0"), State("q1"), State("q2"), State("q3")};
+//
+//	nfa.declareStates(states_vec);
+//
+//	CharacterMatcher c01("a");
+//	CharacterMatcher c12("b");
+//	CharacterMatcher c22("b");
+//	EpsilonMatcher c23;
+//
+//	nfa.addTransition(0, 1, c01);
+//	nfa.addTransition(1, 2, c12);
+//	nfa.addTransition(2, 2, c22);
+//	nfa.addTransition(2, 3, c23);
+//
+//	bool test1 = nfa.compute("abbbbbb");
+//	bool test2 = nfa.compute("aabbbbbb");
+//	bool test3 = nfa.compute("ab");
+//	bool test4 = nfa.compute("a");
+//	bool test5 = nfa.compute("abc");
+//
+//	std::cout << "Test 1: ['abb*' matches 'abbbbbb'?] (should be true): " << test1 << std::endl;
+//	std::cout << "Test 2: ['abb*' matches 'aabbbbbb'?] (should be false): " << test2 << std::endl;
+//	std::cout << "Test 3: ['abb*' matches 'ab'?] (should be true): " << test3 << std::endl;
+//	std::cout << "Test 4: ['abb*' matches 'a'?] (should be false): " << test4 << std::endl;
+//	std::cout << "Test 5: ['abb*' matches 'abc'?](should be false): " << test5 << " -> needs 'memory' feature " << std::endl;
 
-	nfa.declareStates(states_vec);
+	std::vector<State> states_vector{State("q0"), State("q1"), State("q2")};
+	nfa.declareStates(states_vector);
 
-	CharacterMatcher c01("a");
-	CharacterMatcher c12("b");
-	CharacterMatcher c22("b");
-	EpsilonMatcher c23;
+	CharacterMatcher a("a");
+	EpsilonMatcher eps;
+	CharacterMatcher b("b");
 
-	nfa.addTransition(0, 1, c01);
-	nfa.addTransition(1, 2, c12);
-	nfa.addTransition(2, 2, c22);
-	nfa.addTransition(2, 3, c23);
-
-	bool test1 = nfa.compute("abbbbbb");
-	bool test2 = nfa.compute("aabbbbbb");
-	bool test3 = nfa.compute("ab");
-	bool test4 = nfa.compute("a");
-	bool test5 = nfa.compute("abc");
-
-	std::cout << "Test 1: ['abb*' matches 'abbbbbb'?] (should be true): " << test1 << std::endl;
-	std::cout << "Test 2: ['abb*' matches 'aabbbbbb'?] (should be false): " << test2 << std::endl;
-	std::cout << "Test 3: ['abb*' matches 'ab'?] (should be true): " << test3 << std::endl;
-	std::cout << "Test 4: ['abb*' matches 'a'?] (should be false): " << test4 << std::endl;
-	std::cout << "Test 5: ['abb*' matches 'abc'?](should be false): " << test5 << " -> needs 'memory' feature " << std::endl;
+	nfa.addTransition(0, 1, a);
+	nfa.addTransition(1, 1, eps);
+	nfa.addTransition(1, 2, b);
+	std::cout<< "Should return true: " << nfa.compute("ab") << " ... and not be stuck forever" << std::endl;
 }
